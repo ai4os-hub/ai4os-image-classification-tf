1abb1e7cd1025f9ef24524f11bafca6dd8ccf38c
Ignacio Heredia <iheredia@ifca.unican.es>
2024-05-03 16:48:28 +0200

    feat: update old tests

--- imgclas/tests/test_methods.py
+++ imgclas/tests/test_methods.py
@@ -18,7 +18,7 @@ from imgclas import paths
 
 
 module_name = 'imgclas'
-test_url = 'https://file-examples.com/wp-content/uploads/2017/10/file_example_JPG_100kB.jpg'
+test_url = 'https://file-examples.com/storage/fe4996602366316ffa06467/2017/10/file_example_JPG_100kB.jpg'
 
 data_path = os.path.join(paths.get_base_dir(), 'data')
 
@@ -35,11 +35,6 @@ def remove_files(src, extension):
     for file in files:
         if os.path.isfile(file):
             os.remove(file)
-
-
-# ===========
-# Local Tests
-# ===========
 
 def test_load():
     print('Testing local: module load ...')
@@ -95,76 +90,12 @@ def test_train():
 
     # shutil.rmtree(os.path.join(paths.get_models_dir(), out['modelname']), ignore_errors=True)
 
-# ==========
-# CURL Tests
-# ==========
-
-def test_curl_load():
-    print('Testing curl: module load ...')
-
-    r = subprocess.run('curl -X GET "http://0.0.0.0:5000/v2/models/" -H "accept: application/json"',
-                       shell=True, check=True, stdout=subprocess.PIPE).stdout
-    r = json.loads(r)
-    models = [m['name'] for m in r['models']]
-    if module_name not in models:
-        raise Exception('Model is not correctly loaded.')
-
-
-def test_curl_metadata():
-    print('Testing curl: metadata ...')
-
-    r = subprocess.run('curl -X GET "http://0.0.0.0:5000/v2/models/{}/" -H "accept: application/json"'.format(module_name),
-                       shell=True, check=True, stdout=subprocess.PIPE).stdout
-    if r == b'404: Not Found':
-        raise Exception('Model is not correctly loaded.')
-    r = json.loads(r)
-
-
-def test_curl_predict_url():
-    print('Testing curl: predict url ...')
-
-    r = subprocess.run('curl -X POST "http://0.0.0.0:5000/v2/models/{}/predict/?urls={}" -H "accept: application/json"'.format(module_name,
-                                                                                                                               quote_plus(test_url)),
-                       shell=True, check=True, stdout=subprocess.PIPE).stdout
-    if r == b'404: Not Found':
-        raise Exception('Model is not correctly loaded.')
-    r = json.loads(r)
-
-
-def test_curl_train():
-    print('Testing curl: train ...')
-
-    copy_files(src=os.path.join(data_path, 'demo-dataset_files'),
-               dst=os.path.join(data_path, 'dataset_files'),
-               extension='*.txt')
-
-    command = """curl -X POST "http://0.0.0.0:5000/v2/models/imgclas/train/?base_directory=%22.%22&images_directory=%22data%2Fsamples%22&modelname=%22MobileNet%22&image_size=224&num_classes=null&preprocess_mode=null&mode=%22normal%22&initial_lr=0.001&batch_size=16&epochs=15&ckpt_freq=null&lr_schedule_mode=%22step%22&lr_step_decay=0.1&lr_step_schedule=%5B0.7%2C%200.9%5D&l2_reg=0.0001&use_class_weights=false&use_validation=true&use_early_stopping=false&use_multiprocessing=false&use_tensorboard=false&use_remote=false&mean_RGB=null&std_RGB=null&train_mode=%7B%22h_flip%22%3A%200.5%2C%20%22v_flip%22%3A%200.5%2C%20%22rot%22%3A%200.7%2C%20%22rot_lim%22%3A%2090%2C%20%22stretch%22%3A%200.0%2C%20%22crop%22%3A%201.0%2C%20%22zoom%22%3A%200.2%2C%20%22blur%22%3A%200.3%2C%20%22pixel_noise%22%3A%200.3%2C%20%22pixel_sat%22%3A%200.3%2C%20%22cutout%22%3A%200.5%7D&val_mode=%7B%22h_flip%22%3A%200.5%2C%20%22v_flip%22%3A%200.0%2C%20%22rot%22%3A%200.5%2C%20%22rot_lim%22%3A%2030%2C%20%22stretch%22%3A%200.0%2C%20%22crop%22%3A%200.9%2C%20%22zoom%22%3A%200.1%2C%20%22blur%22%3A%200.1%2C%20%22pixel_noise%22%3A%200.1%2C%20%22pixel_sat%22%3A%200.1%2C%20%22cutout%22%3A%200.0%7D" -H "accept: application/json"
-    """
-    r = subprocess.run(command, shell=True, check=True, stdout=subprocess.PIPE).stdout
-
-    # """
-    # We cannot remove the resulting files as DEEPaaS is asynchronous and therefore we cannot know we the training
-    # finishes
-    # """
-    # remove_files(src=os.path.join(data_path, 'dataset_files'),
-    #              extension='*.txt')
-    # shutil.rmtree(os.path.join(paths.get_models_dir(), out['modelname']), ignore_errors=True)
-
 
 if __name__ == '__main__':
-    # print('Testing locally ...')
-    # test_load()
+    test_load()
     # test_metadata()
     # test_predict_url()
     # test_predict_data()
-    #
-    # print('Testing through CURL ...')
-    # r = subprocess.run('deepaas-run --listen-ip 0.0.0.0 --nowarm &', shell=True)  # launch deepaas
-    # time.sleep(20)  # wait for deepaas to be ready
-    # test_curl_load()
-    # test_curl_metadata()
-    # test_curl_predict_url()
-    # r = subprocess.run("kill $(ps aux | grep 'deepaas-run' | awk '{print $2}')", shell=True)   # kill deepaas
 
-    test_train()
-    # test_curl_train()
+    # Train function cannot be run in the same loop as predict due to very poor usage of the config file in api.py
+    # test_train()
